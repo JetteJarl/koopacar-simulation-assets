d285b3175a7cdf47f04131ceedfe7268ff856650
JetteJarl <99565791+JetteJarl@users.noreply.github.com>
2023-02-28 11:11:48 +0100

    Bugfixes, finished Todos, kill processes when quitting.

--- create_dataset.sh
+++ create_dataset.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 source ~/.bashrc
 
+# catch sigint
+control_c() {
+  # Kill simulation
+  pkill ros2
+  pkill gazebo
+  pkill gzserver
+  pkill gzclient
+
+  echo "Keyboard interrupt. All processes are killed."
+
+  return
+}
+
+trap control_c SIGINT
+
+
 # Define variables
 label_path=$1
 if ! [[ -f "$label_path" ]]; then
@@ -8,7 +24,7 @@ if ! [[ -f "$label_path" ]]; then
   return 1
 fi
 
-gazebo_start_time=20
+gazebo_start_time=12
 
 num_poses=21
 num_worlds=2
@@ -18,41 +34,40 @@ path_to_worlds="/home/ubuntu/koopacar-simulation-assets/src/koopacar_simulation/
 world_files=("cone_cluster.world" "track01_circle.world")
 launch_files=("cone_cluster.launch.py" "track01_circle.launch.py")
 
+# Install missing dependencies
+rosdep install -i --from-path src --rosdistro foxy -y
+
+# source package and nodes
+. install/setup.bash
+
+# build
+colcon build
+
+source install/local_setup.bash
+
 # Run every simulation with every position
 for w_ind in $(seq 0 `expr $num_worlds - 1`)
 do
-  # Launch simulation TODO: Choose sim according to world file
-  ros2 launch koopacar_simulation cone_cluster.launch.py &
+  # Launch simulation
+  ros2 launch koopacar_simulation "${launch_files[w_ind]}" &
+
+  # Wait
+  sleep $gazebo_start_time
 
   while read line; do
     IFS=', ' read -r -a pose_array <<< "$line"
 
-    # TODO: Add correct pose (needs to be in Quaternion) change w parameter
     ros2 service call /gazebo/set_entity_state "gazebo_msgs/SetEntityState" "{state: {name: "KoopaCar", \
     pose: {position: {x: "${pose_array[0]}", y: "${pose_array[1]}", z: "${pose_array[2]}"}, \
-    orientation: {x: "${pose_array[3]}", y: "${pose_array[4]}", z: "${pose_array[5]}", w: "${pose_array[3]}"}}}}"
-
-    # Install missing dependencies
-    rosdep install -i --from-path src --rosdistro foxy -y
-
-    # source package and nodes
-    . install/setup.bash
-
-    # build
-    colcon build
-
-    source install/local_setup.bash
-
-    # Wait
-    sleep $gazebo_start_time
+    orientation: {x: "${pose_array[3]}", y: "${pose_array[4]}", z: "${pose_array[5]}", w: "${pose_array[6]}"}}}}"
 
     # make snapshot
     ros2 param set /lidar_collection store_once True
 
     # label new data
-    python3 $label_path -pf /home/ubuntu/koopacar-simulation-assets/poses.txt -i $i -w "$path_to_worlds${world_files[$w_ind]}" -d /home/ubuntu/koopacar-system/data/lidar_perception/new_data_set
+    python3 $label_path -p $line -w "$path_to_worlds${world_files[$w_ind]}" -d /home/ubuntu/koopacar-system/data/lidar_perception/new_data_set
 
-  done < "/home/ubuntu/koopacar-simulation-assets/poses.txt"
+  done < "/home/ubuntu/koopacar-simulation-assets/poses_quaternions.txt"
 
   # Kill simulation
   pkill ros2
